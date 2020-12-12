96c39032b136c71bec9724a50e66f0da8a3dda7f
miihon-ani <george@asatsuki.netgamers.jp>
2020-12-12 16:53:19 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>JIS G 4310対応計算機 ver 0.1</h1>
+  <div class="f-container">
+    <div class="f-item">
+      比重：<input v-model="spgr" type="number" />(kg/mm*m<sup>2</sup>)<br/>
+      板厚：<input v-model="thickness" type="number" />(mm)
+      <hr/>
+      単位質量<br/>
+      計算値：{{ calcUnitMass() }}(kg/m<sup>2</sup>)<br/>
+      有効値：{{ calcPrecision(calcUnitMass(), 4) }}(kg/m<sup>2</sup>)
+    </div>
+    <div class="f-item">
+      幅：<input v-model="width" type="number" />(mm)<br/>
+      長さ：<input v-model="length" type="number" />(mm)
+      <hr/>
+      面積<br/>
+      計算値：{{ calcArea() }}(m<sup>2</sup>)<br/>
+      有効値：{{ calcPrecision(calcArea(), 4) }}(m<sup>2</sup>)
+    </div>
+    <div class="f-item">
+      算出質量(kg)<br/>
+      計算値：{{ calcMass() }}(kg)<br/>
+      有効値：{{ calcPrecision(calcMass(), 3) }}(kg)
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      spgr: 7.93,
+      thickness: 5,
+      width: 1024,
+      length: 2048
+    }
+  },
+  methods: {
+    calcUnitMass() {
+      const BigNumber = require('bignumber.js');
+      return BigNumber(this.spgr).times(this.thickness).toNumber();
+    },
+    calcArea() {
+      const BigNumber = require('bignumber.js');
+      return BigNumber(this.width).times(this.length).div(1000000).toNumber();
+    },
+    calcMass() {
+      const BigNumber = require('bignumber.js');
+      let unitMass = BigNumber(this.spgr).times(this.thickness).toPrecision(4);
+      let area = BigNumber(this.width).times(this.length).div(1000000).toPrecision(4);
+      return BigNumber(unitMass).times(area).toNumber();
+    },
+    // num:number(数字)
+    // sd:significant digits(有効桁数)
+    calcPrecision(num, sd){
+      const BigNumber = require('bignumber.js');
+      return BigNumber(num).toPrecision(sd);
+    }
   }
 }
 </script>
@@ -19,8 +66,13 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.f-container {
+    display: flex;
+}
+.f-item {
+    margin:20px
 }
 </style>
